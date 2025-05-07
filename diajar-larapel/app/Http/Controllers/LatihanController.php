ffd994be5cd11dfbc72c5faa6e046012a1de984e
maheeesaaa <adeekkurtawan@gmail.com>
2025-05-07 14:03:26 +0700

    Initial commit

---
+++ diajar-larapel/app/Http/Controllers/LatihanController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\View\View;
+use App\Models\Latihan;
+use Illuminate\Support\Facades\Storage;
+
+class LatihanController extends Controller
+{
+    public function index(): View
+    {
+        $latihans = Latihan::latest()->paginate(10);
+        return view('latihan.index', compact('latihans'));
+    }
+
+    public function create(): View
+    {
+        return view('latihan.create');
+    }
+
+    public function store(Request $request): RedirectResponse
+    {
+        $this->validate($request, [
+            'image'     => 'required|image|mimes:jpeg,jpg,png|max:2048',
+            'title'     => 'required|min:5',
+            'content'   => 'required|min:10'
+        ]);
+
+        $image = $request->file('image');
+        $image->storeAs('public/latihan', $image->hashName());
+
+        Latihan::create([
+            'image'    => $image->hashName(),
+            'title'    => $request->title,
+            'content'  => $request->content
+        ]);
+
+        return redirect()->route('latihan.index')->with(['success' => 'Data Berhasil Disimpan!']);
+    }
+
+    public function show(string $id): View
+    {
+        $latihan = Latihan::findOrFail($id);
+
+        return view('latihan.show', compact('latihan'));
+    }
+
+    public function edit(string $id): View
+    {
+        $latihan = Latihan::findOrFail($id);
+
+        return view('latihan.edit', compact('latihan'));
+    }
+
+    public function update(Request $request, string $id): RedirectResponse
+    {
+        $this->validate($request, [
+            'image'     =>'image|mimes:jpeg,jpg,png|max:2048',
+            'title'     =>'required|min:5',
+            'content'   =>'required|min:10'
+        ]);
+
+        $latihan = Latihan::findOrFail($id);
+
+        if ($request->hasFile('image')) {
+
+            $image = $request->file('image');
+            $image->storeAs('public/latohans/'.$latihan->hashName());
+
+            Storage::delete('public/latihans/'.$latihan->image);
+
+            $latihan->update([
+                'image'=>$image->hashName(),
+                'title'=>$request->title,
+                'content'=>$request->content
+            ]);
+
+        }else {
+            $latihan->update([
+                'title'=>$request->title,
+                'content'=>$request->content
+            ]);
+        }
+
+        return redirect()->route('latihan.index')->with(['success'=> 'Data Berhasil Diubah!']);
+    }
+
+    public function destroy(string $id): RedirectResponse
+    {
+
+        $latihan = Latihan::findOrFail($id);
+        Storage::delete('public/latihan/'. $latihan->image);
+        $latihan->delete();
+
+        return redirect()->route('latihan.index')->with(['success' => 'Data Berhasil Dihapus!']);
+    }
+}
---
+++ diajar-larapel/resources/views/latihan/index.blade.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Data Latihan</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+</head>
+<body style="background: lightgray">
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-12">
+                <div>
+                    <h3 class="text-center my-4">Tutorial Latihan Laravel</h3>
+                    <hr>
+                </div>
+                <div class="card border-0 shadown-sm rounded">
+                    <div class="card-body">
+                        <a href="{{ route('latihan.create') }}" class="btn btn-md btn-success mb-3">TAMBAH POST</a>
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <th scope="col">GAMBAR</th>
+                                    <th scope="col">JUDUL</th>
+                                    <th scope="col">CONTENT</th>
+                                    <th scope="col">AKSI</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @forelse ($latihans as $latihan)
+                                <tr>
+                                    <td class="text-center">
+                                        <img src="{{ asset('/storage/latihan/'.$latihan->image) }}" class="rounded" style="width: 150px">
+                                    </td>
+                                    <td>{{ $latihan->title }}</td>
+                                    <td>{!! $latihan->content !!}</td>
+                                    <td class="text-center">
+                                        <form onsubmit="return confirm('Apakah Anda Yakin ?');" action="{{ route('latihan.destroy', $latihan->id) }}" method="POST">
+                                            <a href="{{ route('latihan.show', $latihan->id) }}" class="btn btn-sm btn-dark">SHOW</a>
+                                            <a href="{{ route('latihan.edit', $latihan->id) }}" class="btn btn-sm btn-primary">EDIT</a>
+                                            @csrf 
+                                            @method('DELETE')
+                                            <button type="submit" class="btn btn-sm btn-danger">HAPUS</button>
+                                        </form>
+                                    </td>
+                                </tr>
+                                @empty
+                                    <div class="alert alert-danger">
+                                        Data Post belum Tersedia.
+                                    </div>
+                                @endforelse
+                            </tbody>
+                        </table>
+                        {{ $latihans->links() }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+
+    <script>
+        //message with toastr
+        @if(session()->has('success'))
+
+        toastr.success('{{ session('success') }}', 'BERHASIL!');
+
+        @elseif(session()->has('error'))
+
+        toastr.error('{{ session('error') }}', 'GAGAL!');
+
+       @endif
+    </script>
+
+
+
+</body>
+</html>
